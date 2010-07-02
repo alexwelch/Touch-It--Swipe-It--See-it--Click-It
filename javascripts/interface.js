a2d8f1e5f673011751819e2ec0d2757a84dc2da7
Alex Welch <me@alexwelch.com>
2010-07-02 15:54:33 -0600

    pretty transitions

--- javascripts/interface.js
+++ javascripts/interface.js
@@ -1,14 +1,14 @@
 var Page = Class.create({  
-  number : 0, 
-  container_width : 980,
+  number : 0,   
+  total : -1,
+  element : false,
   initialize : function(a) {
     this.element = a;
     this.pages = this.element.select('div.page');
     this.total = this.pages.length;
   },      
   goto : function(number) {
-    lpos = -(number * this.container_width);
-    this.element.style.webkitTransform = "translate3d(" + lpos + "px, 0, 0)";
+    this.number = number;
   },
   restart : function() {
     this.number = 0;
@@ -45,24 +45,46 @@ var TrackTouchMove = Class.create( {
 		x : false,
 		y : false
 	},
-	translatePos : 0,
-	defaultCancelLimit : 30,
-	defaultCancelLimitLarge : 100,
-	rightLimit : 980 * 3,
+	container_x : 0,
+	container_target_x : 0,	
+	cancelLimit : -1,
+	defaultCancelLimitLarge : 600,
+	container_width : 980,
+	defaultCancelLimit : 300,
+	rightLimit : -1,
+	element : false,
+	page : false,
+	is_touching : false,
+	animation_timer : false,
+	frame_rate : 1000/30,
 	initialize : function(a) {
 		this.cancelLimit = this.defaultCancelLimit;
 		this.element = a;
     this.page = new Page(a);
 		this.element.style.webkitTransform = "translate3d(0, 0, 0)";
+		this.rightLimit = this.container_width * (this.page.total - 1);
 		this.element.observe("touchstart", this.onStart.bind(this));
 		this.element.observe("touchmove", this.onMove.bind(this));
 		this.element.observe("touchend", this.onEnd.bind(this));
-		this.element.observe("touchcancel", this.onEnd.bind(this));
+		this.element.observe("touchcancel", this.onEnd.bind(this));		
+		this.runTimer(this);
+	},
+	runTimer : function() {
+	  if (!this.is_touching && this.container_x !== this.container_target_x) {
+      if (Math.abs(this.container_x - this.container_target_x) <= 0.5 ) {        
+        this.container_x = this.container_target_x;
+      }
+	    console.log('is_running');
+      this.container_x -= ( ( this.container_x - this.container_target_x ) / 5 );
+      this.element.style.webkitTransform = "translate3d(" + this.container_x + "px, 0, 0)";      
+	  }
+	  
+	  setTimeout( function(t) { t.runTimer(); } , this.frame_rate, this);
 	},
 	onStart : function(a) {
-		if (a.touches.length === 1) {		  
-		  console.log("starpos: " + this.startPosition);
-			this.startingPosition = this.page.number * -980;
+		if (a.touches.length === 1) {		
+		  this.is_touching = true;  
+			this.startingPosition = this.page.number * -this.container_width;
 			this.touchstart.x = a.touches[0].clientX;
 			this.touchstart.y = a.touches[0].clientY;
 			this.element.addClassName("dragging");
@@ -88,36 +110,34 @@ var TrackTouchMove = Class.create( {
 				this.cancelLimit = this.defaultCancelLimitLarge;
 			}
 			if (!isNaN(this.touchmove.x)) {
-				this.translatePos += this.touchmove.x;
-				if (this.translatePos < 0 && this.translatePos >= -this.rightLimit) {
-          this.element.style.webkitTransform = "translate3d(" + this.translatePos + "px, 0, 0)";
+				this.container_x += this.touchmove.x;
+				if (this.container_x < 0 && this.container_x >= -this.rightLimit) {
+          this.element.style.webkitTransform = "translate3d(" + this.container_x + "px, 0, 0)";
 				} else {
 					this.cancelEvent = true;
-					this.translatePos = this.translatePos >= 0 ? 0 : -this.rightLimit;
+					this.container_x = this.container_x >= 0 ? 0 : -this.rightLimit;
 				}
 			}
 		}
 	},
 	onEnd : function(a) {
 		if (this.touchstart.x !== false && this.touchend.x !== false) {
+		  this.is_touching = false;
 			this.touchdiffx = this.touchend.x - this.touchstart.x;
 			this.element.removeClassName("dragging");
 			if (Math.abs(this.touchdiffx) <= this.cancelLimit) {
 				a.stop();
-				this.translatePos = this.startingPosition;
-				this.element.style.webkitTransform = "translate3d(" + this.startingPosition + "px, 0, 0)";
+				this.container_target_x = this.startingPosition;
 			} else {
 				if (!this.cancelEvent) {
 					if (this.touchdiffx > 0) {
-					  this.page.previous();
-            this.translatePos = this.page.number * -980;            
+					  this.page.previous();            
 					} else {
-					  this.page.next();
-            this.translatePos = this.page.number * -980;
+					  this.page.next();            
 					}
+					this.container_target_x = this.page.number * -this.container_width;            
 				} else {
-					this.translatePos = this.startingPosition;
-					this.element.style.webkitTransform = "translate3d(" + this.startingPosition + "px, 0, 0)";
+					this.container_target_x = this.startingPosition;
 				}
 			}
 		}
